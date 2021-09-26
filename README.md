80b6c34a2e385dff9706400d3ca3ae0f773c20e6
Isaac Maier <padspambox@gmail.com>
2021-09-26 17:55:39 -0400

    ✨ 🚧 Add hacky proof-of-concept for github search with manifest file method of loading data into cards.

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 # spicetify-marketplace
-Download Extensions and Themes Directly from Spicetify
+Download Extensions and Themes Directly from within [Spicetify](https://github.com/khanhas/spicetify-cli)
 
-Based on the reddit Custom App
+Based on the [reddit Custom App](https://github.com/khanhas/spicetify-cli/wiki/Custom-Apps#reddit)
+
+## Basic functionality: 
+1. On `index.js:pageLoad()`, calls `getAllExtensions()`, which queries the GitHub API for any repos with the "spicetify-extension" topic. We'll likely add our own tag in the future, like "spicetify-marketplace". 
+2. The it loops through all the results and runs `fetchExtension()`, which fetches a `manifest.json` file from the repo's root folder. If it finds one, we generate a card based on the info. 
+
+## manifest.json
+In order to show up in the custom app, your repo needs to follow these requirements:
+* Have the matching GitHub tag ("spicetify-extension" currently, will likely change)
+* Have a `manifest.json` in the root folder
+    * `name`: Your extension name
+    * `description`: Description for your extension
+    * `preview`: A path to your preview image. Must be relative to your project root
+    * `main`: The filename for your extension's main js file. Must be relative to your project root
+    
+e.g. 
+```json
+{
+    "name": "Spicetify-Hide-Podcasts",
+    "description": "Spicetify extension to hide podcasts.",
+    "preview": "screenshot.png",
+    "main": "hidePodcasts.js"
+}
+```
+
+## Notes: 
+This is all still very hacky, injecting stuff into the reddit custom app, so there are a lot of leftovers that aren't needed. These will be adjusted/removed later. 
--- index.js
+++ index.js
@@ -135,23 +135,52 @@ class Grid extends react.Component {
         this.newRequest(30);
     }
 
+    
+
     async loadPage(queue) {
-        let subMeta = await getSubreddit(requestAfter);
-        let posts = postMapper(subMeta.data.children);
-        for (const post of posts) {
+        // let subMeta = await getSubreddit(requestAfter);
+
+        let allExtensions = await getAllExtensions();
+        console.log(allExtensions);
+        // foo.items.forEach(async item => {
+        //     // const repo = await getRepo(item.contents_url);
+        //     let manifest = null;
+        //     try {
+        //         manifest = await getRepoManifest(item.contents_url);
+        //         console.log(item.name, manifest);
+        //     } catch (err) {
+        //         console.error(item.name, 'no manifest');
+        //     }
+        // })
+
+        // let posts = postMapper(subMeta.data.children);
+        // for (const post of posts) {
+        //     let item;
+        //     switch (post.type) {
+        //         case "playlist":
+        //         case "playlist-v2":
+        //             // item = await fetchPlaylist(post);
+        //             item = await fetchExtension()
+        //             break;
+        //         case "track":
+        //             item = await fetchTrack(post);
+        //             break;
+        //         case "album":
+        //             item = await fetchAlbum(post);
+        //             break;
+        //     }
+        //     if (requestQueue.length > 1 && queue !== requestQueue[0]) {
+        //         // Stop this queue from continuing to fetch and append to cards list
+        //         return -1;
+        //     }
+
+        //     item && this.appendCard(item);
+        // }
+
+        for (const extension of allExtensions.items) {
             let item;
-            switch (post.type) {
-                case "playlist":
-                case "playlist-v2":
-                    item = await fetchPlaylist(post);
-                    break;
-                case "track":
-                    item = await fetchTrack(post);
-                    break;
-                case "album":
-                    item = await fetchAlbum(post);
-                    break;
-            }
+            item = await fetchExtension(extension.contents_url);
+            console.log(extension.name, item);
             if (requestQueue.length > 1 && queue !== requestQueue[0]) {
                 // Stop this queue from continuing to fetch and append to cards list
                 return -1;
@@ -160,9 +189,10 @@ class Grid extends react.Component {
             item && this.appendCard(item);
         }
 
-        if (subMeta.data.after) {
-            return subMeta.data.after;
-        }
+        // TODO: idk what this does, so don't delete yet
+        // if (subMeta.data.after) {
+        //     return subMeta.data.after;
+        // }
 
         this.setState({ rest: true, endOfList: true });
         endOfList = true;
@@ -265,6 +295,49 @@ class Grid extends react.Component {
     }
 }
 
+// TODO: add license filter or anything?
+// TODO: add sort type, order, paging, etc?
+// https://docs.github.com/en/github/searching-for-information-on-github/searching-on-github/searching-for-repositories#search-by-topic
+// https://docs.github.com/en/rest/reference/search#search-repositories
+/**
+ * Query GitHub for all repos with the "spicetify-extensions" topic
+ * @returns Array of search results
+ */
+async function getAllExtensions() {
+    // www is needed or it will block with "cross-origin" error.
+    let url = `https://api.github.com/search/repositories?q=${encodeURIComponent('topic:spicetify-extensions')}`
+    
+    // TODO: idk what this is, so don't delete yet
+    // if (after) {
+    //     url += `&after=${after}`
+    // }
+    // if (sortConfig.by.match(/top|controversial/) && sortConfig.time) {
+    //     url += `&t=${sortConfig.time}`
+    // }
+
+    return await Spicetify.CosmosAsync.get(url);
+}
+
+// e.g. "https://api.github.com/repos/theRealPadster/spicetify-hide-podcasts/contents/{+path}"
+async function getRepoContents(contents_url) {
+    const url = contents_url.replace('{+path}', '');
+    return await Spicetify.CosmosAsync.get(url);
+}
+
+// TODO: add try/catch here? 
+// TODO: can we add a return type here?
+/**
+ * Get the manifest object for a repo
+ * @param {string} user Owner username
+ * @param {string} repo Repo name
+ * @param {string} branch Default branch name (e.g. main or master)
+ * @returns The manifest object
+ */
+async function getRepoManifest(user, repo, branch) {
+    const url = `https://raw.githubusercontent.com/${user}/${repo}/${branch}/manifest.json`;
+    return await Spicetify.CosmosAsync.get(url);
+}
+
 async function getSubreddit(after = '') {
     // www is needed or it will block with "cross-origin" error.
     var url = `https://www.reddit.com/r/${CONFIG.lastService}/${sortConfig.by}.json?limit=100&count=10&raw_json=1`
@@ -276,6 +349,43 @@ async function getSubreddit(after = '') {
     }
 
     return await Spicetify.CosmosAsync.get(url);
+}
+
+// TODO: can we add a return type here?
+/**
+ * Fetch an extension and format data for generating a card
+ * @param {string} contents_url The repo's GitHub API contents_url (e.g. "https://api.github.com/repos/theRealPadster/spicetify-hide-podcasts/contents/{+path}")
+ * @returns Extension info for card (or null)
+ */
+async function fetchExtension(contents_url) {
+    try {
+        // TODO: use the original search full_name ("theRealPadster/spicetify-hide-podcasts") or something to get the url better?
+        const regex_result = contents_url.match(/https:\/\/api\.github\.com\/repos\/(?<user>.+)\/(?<repo>.+)\/contents/);
+        // TODO: err handling?
+        if (!regex_result || !regex_result.groups) return null;
+        const { user, repo } = regex_result.groups;
+        
+        // TODO: how do i get the default branch name?
+        const manifest = await getRepoManifest(user, repo, 'main');
+        console.log(`${user}/${repo}`, manifest);
+
+        return ({
+            // These are required for the reddit extension. We'll remove what we don't need once we understand how it works better
+            type: typesLocale.playlist,
+            uri: 'spotify:user:therealpadster:playlist:4L4WIps1CJ8WU089ii38Cq',
+            upvotes: 0,
+            followersCount: 0,
+
+            // This is the actual stuff we want
+            title: manifest.name,
+            subtitle: manifest.description,
+            imageURL: `https://raw.githubusercontent.com/${user}/${repo}/main/${manifest.preview}`,
+        });
+    } catch (err) {
+        console.warn(contents_url, err);
+        // console.error(contents_url, 'no manifest');
+        return null;
+    }
 }
 
 async function fetchPlaylist(post) {
