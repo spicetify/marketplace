332483a66966a6489f5bd392f31901995240dd5e
Isaac Maier <padspambox@gmail.com>
2021-10-15 17:36:43 -0400

    🚧 Work on extension init

--- Extension.js
+++ Extension.js
@@ -9,14 +9,54 @@
 /// <reference path="../spicetify-cli/globals.d.ts" />
 
 (function MarketplaceExtension() {
-    // TODO: this is pulled from HidePodcasts, we'll need to adjust
-    const { Player, Menu, LocalStorage, Platform } = Spicetify;
-    const main = document.querySelector(".main-view-container__scroll-node-child");
-    if (!(Player && Menu && LocalStorage && Platform && main)) {
+    const { LocalStorage } = Spicetify;
+    if (!(LocalStorage)) {
         // console.log('Not ready, waiting...');
         setTimeout(MarketplaceExtension, 1000);
         return;
     }
 
+    // TODO: can we reference/require/import common files between extension and custom app?
+    const LOCALSTORAGE_KEYS = {
+        "installedExtensions": "marketplace:installed-extensions",
+        "activeTab": "marketplace:active-tab",
+        "tabs": "marketplace:tabs",
+    };
+
+    const getInstalledExtensions = () => {
+        const installedExtensionsStr = LocalStorage.get(LOCALSTORAGE_KEYS.installedExtensions) || "[]";
+        const installedExtensions = JSON.parse(installedExtensionsStr);
+        return installedExtensions;
+    };
+
+    const getExtensionFromKey = (key) => {
+        const manifestStr = LocalStorage.get(key);
+        if (!manifestStr) return null;
+
+        const manifest = JSON.parse(manifestStr);
+        return manifest;
+    };
+
+    const initializeExtension = (extensionKey) => {
+        const extensionManifest = getExtensionFromKey(extensionKey);
+        if (!extensionManifest) return;
+
+        console.log("Initializing extension: ", extensionManifest);
+        console.log("TODO: not implemented (need to get user, repo, and branch - or init differently)");
+
+        // const script = document.createElement("script");
+        // script.defer = true;
+
+        // if (main[0]  === "/") main = main.slice(1);
+
+        // script.src = `https://cdn.jsdelivr.net/gh/${user}/${repo}@${branch}/${main}`;
+        // document.body.appendChild(script);
+    };
+
     console.log("Loaded Marketplace extension");
+
+    const installedExtensions = getInstalledExtensions();
+    console.log("installedExtensions", installedExtensions);
+
+    installedExtensions.forEach((extensionKey) => initializeExtension(extensionKey));
 })();
