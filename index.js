68bdbb7743994a690464fd309ca94ba5ab74705d
CharlieS1103 <someonelikeswindows@gmail.com>
2021-11-18 16:07:40 -0500

    Remove utils, can't figure it out

--- index.js
+++ index.js
@@ -8,7 +8,6 @@
 /// <reference path="src/SortBox.js" />
 /// <reference path="src/TabBar.js" />
 /// <reference path="src/ReadmePage.js" />
-/// <reference path="src/Utils.js" />
 
 /* eslint-disable no-redeclare, no-unused-vars */
 /** @type {React} */
@@ -674,3 +673,20 @@ function generateSchemesOptions(schemes) {
     // ]
     return Object.keys(schemes).map(schemeName => ({ key: schemeName, value: schemeName }));
 }
+const hexToRGB = (hex) => {
+    if (hex.length === 3) {
+        hex = hex.split("").map((char) => char + char).join("");
+    } else if (hex.length != 6) {
+        throw "Only 3- or 6-digit hex colours are allowed.";
+    } else if (hex.match(/[^0-9a-f]/i)) {
+        throw "Only hex colours are allowed.";
+    }
+
+    const aRgbHex = hex.match(/.{1,2}/g);
+    const aRgb = [
+        parseInt(aRgbHex[0], 16),
+        parseInt(aRgbHex[1], 16),
+        parseInt(aRgbHex[2], 16),
+    ];
+    return aRgb;
+};
