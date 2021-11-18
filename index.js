45abe614bee50ea3a08d2bb4d8cfcfc233e5340b
CharlieS1103 <someonelikeswindows@gmail.com>
2021-11-18 11:12:29 -0500

    Move Readmepages, add hextorgb to utils.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@
 /// <reference path="src/Settings.js" />
 /// <reference path="src/SortBox.js" />
 /// <reference path="src/TabBar.js" />
-/// <reference path="ReadmePage.js" />
+/// <reference path="src/ReadmePage.js" />
+/// <reference path="src/Utils.js" />
 
 /* eslint-disable no-redeclare, no-unused-vars */
 /** @type {React} */
@@ -29,25 +30,6 @@ const LOCALSTORAGE_KEYS = {
     "tabs": "marketplace:tabs",
     "sortBy": "marketplace:sort-by",
     "themeInstalled": "marketplace:theme-installed",
-};
-
-// TODO: move this
-const hexToRGB = (hex) => {
-    if (hex.length === 3) {
-        hex = hex.split("").map((char) => char + char).join("");
-    } else if (hex.length != 6) {
-        throw "Only 3- or 6-digit hex colours are allowed.";
-    } else if (hex.match(/[^0-9a-f]/i)) {
-        throw "Only hex colours are allowed.";
-    }
-
-    const aRgbHex = hex.match(/.{1,2}/g);
-    const aRgb = [
-        parseInt(aRgbHex[0], 16),
-        parseInt(aRgbHex[1], 16),
-        parseInt(aRgbHex[2], 16),
-    ];
-    return aRgb;
 };
 
 // Define a function called "render" to specify app entry point
---
+++ src/Utils.js
@@ -0,0 +1,17 @@
+const hexToRGB = (hex) => {
+    if (hex.length === 3) {
+        hex = hex.split("").map((char) => char + char).join("");
+    } else if (hex.length != 6) {
+        throw "Only 3- or 6-digit hex colours are allowed.";
+    } else if (hex.match(/[^0-9a-f]/i)) {
+        throw "Only hex colours are allowed.";
+    }
+
+    const aRgbHex = hex.match(/.{1,2}/g);
+    const aRgb = [
+        parseInt(aRgbHex[0], 16),
+        parseInt(aRgbHex[1], 16),
+        parseInt(aRgbHex[2], 16),
+    ];
+    return aRgb;
+};
