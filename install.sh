38701c706561a4ee8a6ccf0c6a21238c9b8c7ad8
theRealPadster <padspambox@gmail.com>
2022-01-17 17:50:12 -0500

    Update install.sh to use $SPICETIFY_CONFIG, be more efficient

--- install.sh
+++ install.sh
@@ -1,53 +1,52 @@
 #!/bin/sh
 # Copyright 2019 khanhas. GPL license.
 # Edited from project Denoland install script (https://github.com/denoland/deno_install)
-set -e
 
+set -e
 
     latest_release_uri="https://github.com/CharlieS1103/spicetify-marketplace/archive/refs/heads/main.zip"
     echo "DOWNLOADING    $latest_release_uri"
 
     download_uri="https://github.com/CharlieS1103/spicetify-marketplace/archive/refs/heads/main.zip"
 
-spicetify_install="${SPICETIFY_INSTALL:-$HOME/spicetify-cli/CustomApps}"
+INSTALL_DIR="$SPICETIFY_CONFIG_DIR/CustomApps"
 
-if [ ! -d "$spicetify_install" ]; then
-    echo "MAKING FOLDER  $spicetify_install";
-    mkdir -p "$spicetify_install"
+if [ ! -d "$INSTALL_DIR" ]; then
+    echo "MAKING FOLDER  $INSTALL_DIR";
+    mkdir -p "$INSTALL_DIR"
 fi
 
-tar_file="$spicetify_install/.zip"
+TAR_FILE="$INSTALL_DIR/spicetify-marketplace-main.zip"
 
 echo "DOWNLOADING   $download_uri"
-curl --fail --location --progress-bar --output "$tar_file" "$download_uri"
-cd "$spicetify_install"
+curl --fail --location --progress-bar --output "$TAR_FILE" "$download_uri"
+cd "$INSTALL_DIR"
 
 echo "EXTRACTING"
-unzip -q -d "$spicetify_install/spicetify-marketplace" -o "$tar_file"
+unzip -q -d "$INSTALL_DIR/spicetify-marketplace-tmp" -o "$TAR_FILE"
 
 echo "REMOVING"
-rm "$tar_file"
+rm "$TAR_FILE"
 
+cd "$INSTALL_DIR/spicetify-marketplace-tmp"
 # Check ~\.spicetify.\CustomApps directory already exists
-cd "$spicetify_install/spicetify-marketplace"
-sp_dot_dir="$(dirname "$(../../spicetify -c)")/CustomApps/spicetify-marketplace"
-if [ ! -d "$sp_dot_dir" ]; then
-    echo "MAKING FOLDER  $sp_dot_dir";
-    mkdir -p "$sp_dot_dir"
-fi
+# sp_dot_dir="$(dirname "$(../../spicetify -c)")/CustomApps/spicetify-marketplace"
+# if [ ! -d "$sp_dot_dir" ]; then
+#     echo "MAKING FOLDER  $sp_dot_dir";
+#     mkdir -p "$sp_dot_dir"
+# fi
 echo "COPYING"
-echo "$sp_dot_dir"
-cp -R "$spicetify_install/spicetify-marketplace/spicetify-marketplace-main" "$spicetify_install/spicetify-marketplace-placeholder"
-rm -r "$spicetify_install/spicetify-marketplace/"
-mv "$spicetify_install/spicetify-marketplace-placeholder/" "$spicetify_install/spicetify-marketplace/"
-cp -rf "$spicetify_install/spicetify-marketplace/." "$sp_dot_dir"
-rm -r "$spicetify_install/spicetify-marketplace/"
+# echo "$sp_dot_dir"
+rm -rf "$INSTALL_DIR/spicetify-marketplace/"
+mv "$INSTALL_DIR/spicetify-marketplace-tmp/spicetify-marketplace-main" "$INSTALL_DIR/spicetify-marketplace"
+# cp -rf "$INSTALL_DIR/spicetify-marketplace/." "$sp_dot_dir"
 echo "INSTALLING"
-cd "$spicetify_install/spicetify-marketplace"
-if ../../spicetify config custom_apps spicetify-marketplace ; then
+cd "$INSTALL_DIR/spicetify-marketplace"
+
+if spicetify config custom_apps spicetify-marketplace ; then
     echo "Added to config!"
     echo "APPLYING"
-    ../../spicetify apply
+    spicetify apply
 else
     echo "Command failed"
     echo "Please run \`spicetify config custom_apps spicetify-marketplace\` manually "
