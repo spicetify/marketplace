48a5185a27006cbe88e18eee705e2a95bc9cf8e4
Isaac Maier <padspambox@gmail.com>
2022-05-31 20:36:26 -0400

    ♻️ Add Toggle component

--- src/components/Modals/Settings/SettingsToggle.tsx
+++ src/components/Modals/Settings/SettingsToggle.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Config } from "../../../types/marketplace-types";
 
 import styles from "../../../styles/modules/toggle.module.scss";
-// import Toggle from "../../Toggle";
+import Toggle from "../../Toggle";
 
 const SettingsToggle = (props: {
   name: string;
@@ -10,7 +10,6 @@ const SettingsToggle = (props: {
   modalConfig: Config;
   clickable?: boolean;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  // updateConfig: (CONFIG: Config) => void;
 }) => {
   const toggleId = `toggle:${props.storageKey}`;
   const enabled = !!props.modalConfig.visual[props.storageKey];
@@ -18,22 +17,11 @@ const SettingsToggle = (props: {
   const wrapperClassList = [styles["toggle-wrapper"]];
   if (!props.clickable === false) wrapperClassList.push(styles.disabled);
 
-  // <Toggle name={props.name} storageKey={props.storageKey} modalConfig={} />
   return (
     <div className='setting-row'>
       <label htmlFor={toggleId} className='col description'>{props.name}</label>
       <div className='col action'>
-        <label className={wrapperClassList.join(" ")}>
-          <input className={styles["toggle-input"]} type='checkbox' checked={enabled}
-            data-storage-key={props.storageKey}
-            id={toggleId}
-            title={`Toggle for ${props.storageKey}`}
-            onChange={props.onChange}
-          />
-          <span className={styles["toggle-indicator-wrapper"]}>
-            <span className={styles["toggle-indicator"]}></span>
-          </span>
-        </label>
+        <Toggle name='Stars count' storageKey='stars' enabled={enabled} onChange={props.onChange} />
       </div>
     </div>
   );
