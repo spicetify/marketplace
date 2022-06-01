2adec9d8c9350091ee32ac6c5c76e99bfd2b0946
Isaac Maier <padspambox@gmail.com>
2022-05-31 21:18:39 -0400

    🚧 Add (non-working) tab reordering function

--- src/components/Modals/Settings/TabRow.tsx
+++ src/components/Modals/Settings/TabRow.tsx
@@ -32,41 +32,39 @@ const TabRow = (props: {
     // stackTabElements();
   };
 
-  // TODO: add this in
-  // function posCallback(currPos, dir) {
-  //   // const el = document.querySelector(`[data-id="${tabName}"]`);
-  //   console.log({ currPos, dir });
-  //   // const id = el.dataset.id;
-  //   // const curPos = modalCONFIG.tabs.findIndex(({ name }) => name === id);
-  //   const newPos = currPos + dir;
+  // TODO: for some reason it just closes the modal when you click it...
+  // Ohh. The app state changes, so it re-renders the entire marketplace app.
+  // That's not ideal. Hmm.
+  function moveTab(currPos, dir) {
+    console.log({ currPos, dir });
 
-  //   const temp = modalConfig.tabs[newPos];
-  //   modalConfig.tabs[newPos] = modalConfig.tabs[currPos];
-  //   modalConfig.tabs[currPos] = temp;
+    const newPos = currPos + dir;
 
-  //   localStorage.setItem(
-  //     LOCALSTORAGE_KEYS.tabs,
-  //     JSON.stringify(modalConfig.tabs),
-  //   );
+    const temp = props.modalConfig.tabs[newPos];
+    props.modalConfig.tabs[newPos] = props.modalConfig.tabs[currPos];
+    props.modalConfig.tabs[currPos] = temp;
 
-  //   // TODO: does this work?
-  //   updateAppConfig(modalConfig);
+    localStorage.setItem(
+      LOCALSTORAGE_KEYS.tabs,
+      JSON.stringify(props.modalConfig.tabs),
+    );
 
-  //   // stackTabElements();
-  // }
+    // Saves the config settings to app as well as SettingsModal state
+    props.updateConfig(props.modalConfig);
 
-  const posCallback = () => 1;
+    // stackTabElements();
+  }
 
   return (
     <div className="setting-row" key={props.name}>
       <label htmlFor={toggleId} className='col description'>{props.name}</label>
       <div className="col action">
-        <button title="Move up" className="arrow-btn" onClick={() => posCallback(index, -1)}>
+        <button title="Move up" className="arrow-btn" onClick={() => moveTab(index, -1)}>
           <svg height="16" width="16" viewBox="0 0 16 16" fill="currentColor"
             dangerouslySetInnerHTML={{ __html: String(Spicetify.SVGIcons["chart-up"]) }}>
           </svg>
         </button>
-        <button title="Move down" className="arrow-btn" onClick={() => posCallback(index, 1)}>
+        <button title="Move down" className="arrow-btn" onClick={() => moveTab(index, 1)}>
           <svg height="16" width="16" viewBox="0 0 16 16" fill="currentColor"
             dangerouslySetInnerHTML={{ __html: String(Spicetify.SVGIcons["chart-down"]) }}>
           </svg>
