632f82ce278303ea95188e1c6fca4dbeda066215
Nam Anh <phamnamanh25@gmail.com>
2022-06-09 22:52:52 +0700

    feat(Snippet): preview & edit snippets (#225)
    * feat(Snippet): preview & edit snippets
    
    * chore: remove comments

--- src/components/Modals/Snippet/index.tsx
+++ src/components/Modals/Snippet/index.tsx
@@ -5,11 +5,13 @@ import {
 } from "../../../logic/Utils";
 import { LOCALSTORAGE_KEYS } from "../../../constants";
 import Button from "../../Button";
+import { CardProps } from "../../Card/Card";
+import { ModalType } from "../../../logic/LaunchModals";
 
-const AddSnippetModal = () => {
-  const [code, setCode] = React.useState("");
-  const [name, setName] = React.useState("");
-  const [description, setDescription] = React.useState("");
+const SnippetModal = (props: { content?: CardProps, type: ModalType }) => {
+  const [code, setCode] = React.useState(props.type === "ADD_SNIPPET" ? "" : props.content?.item.code || "");
+  const [name, setName] = React.useState(props.type === "ADD_SNIPPET" ? "" : props.content?.item.title || "");
+  const [description, setDescription] = React.useState(props.type === "ADD_SNIPPET" ? "" : props.content?.item.description || "");
 
   const saveSnippet = () => {
     const processedCode = code.replace(/\n/g, "");
@@ -17,18 +19,29 @@ const AddSnippetModal = () => {
     const processedDescription = description.trim();
 
     const localStorageKey = `marketplace:installed:snippet:${processedName}`;
-    if (getLocalStorageDataFromKey(localStorageKey)) {
-      alert("That name is already taken!");
+    if (getLocalStorageDataFromKey(localStorageKey) && props.type !== "EDIT_SNIPPET") {
+      Spicetify.showNotification("That name is already taken!");
       return;
     }
 
     console.log(`Installing snippet: ${processedName}`);
+    if (props.content && props.content.item.title !== processedName) {
+      // Remove from installed list
+      console.log(`Deleting outdated snippet: ${props.content.item.title}`);
+
+      localStorage.removeItem(`marketplace:installed:snippet:${props.content.item.title}`);
+      const installedSnippetKeys = getLocalStorageDataFromKey(LOCALSTORAGE_KEYS.installedSnippets, []);
+      const remainingInstalledSnippetKeys = installedSnippetKeys.filter((key: string) => key !== `marketplace:installed:snippet:${props.content?.item.title}`);
+      localStorage.setItem(LOCALSTORAGE_KEYS.installedSnippets, JSON.stringify(remainingInstalledSnippetKeys));
+    }
+
     localStorage.setItem(
       localStorageKey,
       JSON.stringify({
         code: processedCode,
         description: processedDescription,
         title: processedName,
+        custom: true,
       }),
     );
 
@@ -39,6 +52,7 @@ const AddSnippetModal = () => {
     );
     if (installedSnippetKeys.indexOf(localStorageKey) === -1) {
       installedSnippetKeys.push(localStorageKey);
+      console.log(installedSnippetKeys);
       localStorage.setItem(
         LOCALSTORAGE_KEYS.installedSnippets,
         JSON.stringify(installedSnippetKeys),
@@ -50,6 +64,7 @@ const AddSnippetModal = () => {
     initializeSnippets(installedSnippets);
 
     Spicetify.PopupModal.hide();
+    if (props.type === "EDIT_SNIPPET") location.reload();
   };
 
   return (
@@ -58,14 +73,20 @@ const AddSnippetModal = () => {
         <label htmlFor="marketplace-custom-css">Custom CSS</label>
         <textarea id="marketplace-custom-css"
           rows={4} cols={50}
-          value={code} onChange={(e) => setCode(e.target.value)}
+          value={code} onChange={(e) => {
+            if (props.type !== "VIEW_SNIPPET")
+              setCode(e.target.value);
+          }}
           placeholder="Input your own custom CSS here! You can find them in the installed tab for management."
         />
       </div>
       <div className="marketplace-customCSS-input-container">
         <label htmlFor="marketplace-customCSS-name-submit">Snippet Name</label>
         <input id="marketplace-customCSS-name-submit"
-          value={name} onChange={(e) => setName(e.target.value)}
+          value={name} onChange={(e) => {
+            if (props.type !== "VIEW_SNIPPET")
+              setName(e.target.value);
+          }}
           placeholder="Enter a name for your custom snippet."
         />
       </div>
@@ -74,15 +95,19 @@ const AddSnippetModal = () => {
           Snippet Description
         </label>
         <input id="marketplace-customCSS-description-submit"
-          value={description} onChange={(e) => setDescription(e.target.value)}
+          value={description} onChange={(e) => {
+            if (props.type !== "VIEW_SNIPPET")
+              setDescription(e.target.value);
+          }}
           placeholder="Enter a description for your custom snippet."
         />
       </div>
-      <Button onClick={saveSnippet}>
-        Save CSS
-      </Button>
+      {props.type !== "VIEW_SNIPPET"
+        ? <Button onClick={saveSnippet}>
+          Save CSS
+        </Button>
+        : <></>}
     </div>
   );
 };
-
-export default AddSnippetModal;
+export default SnippetModal;
