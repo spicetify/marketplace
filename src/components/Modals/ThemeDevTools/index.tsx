7b08bff49c4ad9362427905d9b490fa8e7587fd4
Isaac Maier <padspambox@gmail.com>
2022-06-19 22:44:40 -0400

    Make styling a little more consistent

--- src/components/Modals/ThemeDevTools/index.tsx
+++ src/components/Modals/ThemeDevTools/index.tsx
@@ -1,30 +1,36 @@
 import React from "react";
+import Button from "../../Button";
 import { getInvalidCSS, getLocalStorageDataFromKey, unparseIni, parseIni } from "../../../logic/Utils";
 import { LOCALSTORAGE_KEYS } from "../../../constants";
 
-const themeKey  = localStorage.getItem(LOCALSTORAGE_KEYS.themeInstalled);
-let themeManifest;
-themeKey ? themeManifest = getLocalStorageDataFromKey(themeKey) : null;
+const themeKey = localStorage.getItem(LOCALSTORAGE_KEYS.themeInstalled);
+const themeManifest = themeKey
+  ? getLocalStorageDataFromKey(themeKey)
+  : null;
 
 const ThemeDevToolsModal = () => {
 
   return (
     <div id="marketplace-theme-dev-tools-container" className="marketplace-theme-dev-tools-container">
-      <div className="color-picker-container">
-        <h2 className="devtools-heading">Color.ini Editor</h2>
-        <textarea className="color-ini-editor" id="color-ini-editor">{themeManifest ? unparseIni(themeManifest.schemes) : "Error, no marketplace theme installed."}</textarea>
+      <div className="devtools-column">
+        <label htmlFor="color-ini-editor">
+          <h2 className="devtools-heading">Color.ini Editor</h2>
+        </label>
+        <textarea className="color-ini-editor" id="color-ini-editor">
+          {themeManifest ? unparseIni(themeManifest.schemes) : "Error, no marketplace theme installed."}
+        </textarea>
 
-        <button className="color-ini-editor-save-button" id="color-ini-editor-save-button" onClick={saveColorIni}>Save</button>
+        <Button onClick={saveColorIni}>
+          Save
+        </Button>
       </div>
       {/* Create a box containing the invalid css classnames fetched from "getInvalidCSS()"*/}
-      <div className="invalid-css-container">
+      <div className="devtools-column">
         <h2 className="devtools-heading">Invalid CSS</h2>
 
         {getInvalidCSS().map((cssClass, index) => {
-          {/* TODO: Should probably not use <br>*/}
           return <div key={index} className="invalid-css-text">{cssClass}</div>;
-        },
-        )}
+        })}
 
       </div>
     </div>
@@ -40,7 +46,6 @@ const saveColorIni = () => {
   } else {
     Spicetify.showNotification("Error: No theme manifest found");
   }
-
 };
 
 export default ThemeDevToolsModal;
