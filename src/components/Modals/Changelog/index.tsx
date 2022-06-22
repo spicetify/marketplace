4deb3b117dc4b3926336fde34b11a06c9ba74408
Nam Anh <phamnamanh25@gmail.com>
2022-06-22 09:19:46 +0700

    feat(Changelog): use GitHub gfm for body

--- src/components/Modals/Changelog/index.tsx
+++ src/components/Modals/Changelog/index.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import whatsNew from "spcr-whats-new";
-import ReactMarkdown from "react-markdown";
 import { RELEASE_CHANGELOG, MARKETPLACE_VERSION } from "../../../constants";
 
 const fetchReleaseBody = async () => {
   return fetch(RELEASE_CHANGELOG)
     .then((res) => res.json())
-    .then((result) => {
+    .then(async (result) => {
       // If API returns no error message then get release body
-      const body = !result.message
-        ? result.body
-        : null;
+      const body = !result.message ? result.body as string : null;
+      if (body) return await getReadmeHTML(body);
       return body;
     })
     .catch((err) => {
@@ -19,18 +17,39 @@ const fetchReleaseBody = async () => {
     });
 };
 
+const getReadmeHTML = async (readmeText: string) => {
+  try {
+    const postBody = {
+      text: readmeText,
+      context: `spicetify/spicetify-marketplace`,
+      mode: "gfm",
+    };
+
+    const readmeHtmlRes = await fetch("https://api.github.com/markdown", {
+      method: "POST",
+      body: JSON.stringify(postBody),
+    });
+    if (!readmeHtmlRes.ok)
+      throw Spicetify.showNotification(
+        `Error parsing README (HTTP ${readmeHtmlRes.status})`,
+      );
+
+    const readmeHtml = await readmeHtmlRes.text();
+    return readmeHtml;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
 const Changelog = async () => {
   const changelogBody = await fetchReleaseBody();
   // If a release is not found then don't display changelog modal
   if (!changelogBody) return;
-  whatsNew(
-    "marketplace",
-    MARKETPLACE_VERSION,
-    {
-      title: `✨ Marketplace v${MARKETPLACE_VERSION}`,
-      content: <ReactMarkdown>{changelogBody}</ReactMarkdown>,
-      isLarge: true,
-    },
-  );
+  whatsNew("marketplace", MARKETPLACE_VERSION, {
+    title: `✨ Marketplace v${MARKETPLACE_VERSION}`,
+    content: <div dangerouslySetInnerHTML={{ __html: changelogBody }}></div>,
+    isLarge: true,
+  });
 };
 export default Changelog;
