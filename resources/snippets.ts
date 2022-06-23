7ff3a5387ef379525984fea022678dedd7d1fe44
Nam Anh <phamnamanh25@gmail.com>
2022-06-23 16:55:11 +0700

    style: prettify

--- resources/snippets.ts
+++ resources/snippets.ts
@@ -68,7 +68,7 @@ export default [
   {
     "title": "Fix progress bar displacement when listening from a different Device",
     "description": "Fixes the progress bar displacement for a few Themes. Better Version of @CharlieS1103 Snippet",
-    "code": ".main-connectBar-connectBar { overflow: visible !important; position: absolute !important; display: flex !important; align-items: unset !important; left: 80% !important; height: 20px !important; bottom: 1% !important; padding: 2px !important; background-color: transparent !important; color: var(--spice-text) !important; } .control-button::after { display: none !important; }",
+    "code": ".main-connectBar-connectBar {\n  overflow: visible !important;\n  position: absolute !important;\n  display: flex !important;\n  align-items: unset !important;\n  left: 80% !important;\n  height: 20px !important;\n  bottom: 1% !important;\n  padding: 2px !important;\n  background-color: transparent !important;\n  color: var(--spice-text) !important;\n}\n.control-button::after {\n  display: none !important;\n}",
     "preview": "resources/assets/snippets/fix-progress-bar.png"
   },
 ];
