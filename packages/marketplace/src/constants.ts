70b74bb517485192f4c683c2a7ea5f946dc51100
ririxi <dev@ririxi.dev>
2022-06-29 23:10:18 +0200

    style(constants): fix eslint errors

--- packages/marketplace/src/constants.ts
+++ packages/marketplace/src/constants.ts
@@ -4,25 +4,25 @@ import { version } from "../package.json";
 export const MARKETPLACE_VERSION = version;
 
 export const LOCALSTORAGE_KEYS = {
-	installedExtensions: "marketplace:installed-extensions",
-	installedSnippets: "marketplace:installed-snippets",
-	installedThemes: "marketplace:installed-themes",
-	activeTab: "marketplace:active-tab",
-	tabs: "marketplace:tabs",
-	sortBy: "marketplace:sort-by",
-	// Theme installed store the localsorage key of the theme (e.g. marketplace:installed:NYRI4/Comfy-spicetify/user.css)
-	themeInstalled: "marketplace:theme-installed",
-	colorShift: "marketplace:colorShift"
+  installedExtensions: "marketplace:installed-extensions",
+  installedSnippets: "marketplace:installed-snippets",
+  installedThemes: "marketplace:installed-themes",
+  activeTab: "marketplace:active-tab",
+  tabs: "marketplace:tabs",
+  sortBy: "marketplace:sort-by",
+  // Theme installed store the localsorage key of the theme (e.g. marketplace:installed:NYRI4/Comfy-spicetify/user.css)
+  themeInstalled: "marketplace:theme-installed",
+  colorShift: "marketplace:colorShift",
 };
 
 // Initalize topbar tabs
 // Data initalized in TabBar.js
 export const ALL_TABS: TabItemConfig[] = [
-	{ name: "Extensions", enabled: true },
-	{ name: "Themes", enabled: true },
-	{ name: "Snippets", enabled: true },
-	{ name: "Apps", enabled: true },
-	{ name: "Installed", enabled: true }
+  { name: "Extensions", enabled: true },
+  { name: "Themes", enabled: true },
+  { name: "Snippets", enabled: true },
+  { name: "Apps", enabled: true },
+  { name: "Installed", enabled: true },
 ];
 
 // Max GitHub API items per page
