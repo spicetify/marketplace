8de6f1c2b679c552e9a837c86a8f96d60e81a7e3
CharlieS1103 <someonelikeswindows@gmail.com>
2022-06-28 23:07:22 -0400

    Add TS support

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app.get("/", (req, res) => {
+app.get("/", (req: any, res: { send: (arg0: string) => void; }) => {
     res.send("Hello world!");
 });
 
