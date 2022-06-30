da55bc63ce61adc39bd588e1bcd1302041cef343
ririxi <dev@ririxi.dev>
2022-06-30 13:09:42 +0200

    style: use eslint on marketplace only (#265)

--- packages/backend/src/server/config.ts
+++ packages/backend/src/server/config.ts
@@ -2,5 +2,5 @@ import { config } from "dotenv";
 import debug from "debug";
 config({ path: "./.env" });
 
-export const logger: debug.Debugger = debug("spicetify:backend");
-export const port = Number(process.env.PORT) || 3000;
+export const logger: debug.Debugger = debug("spicetify:backend"),
+	port = Number(process.env.PORT) || 3000;
