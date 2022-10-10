6a675e240b18ff02823a9d845e3c25b60db47cf4
ItsRemix <63855221+ItsRemix@users.noreply.github.com>
2022-10-10 11:46:37 +0200

    Update snippets.ts

--- packages/marketplace/src/resources/snippets.ts
+++ packages/marketplace/src/resources/snippets.ts
@@ -156,9 +156,9 @@ export default [
     "preview": "resources/assets/snippets/remove-ep-likes.png",
   },
   {
-    "title": "Vinyl Cover Art",
+    "title": "Rotating Cover Art",
     "description": "Adds circular mask to cover art and rotation",
     "code": `@keyframes rotating { from { transform: rotate(0deg); } to { transform: rotate(360deg); }} .cover-art { animation: rotating 2s linear infinite; clip-path: circle(50% at 50% 50%);}`,
     "preview": "resources/assets/snippets/rotating-coverart"
-  }
+  },
 ];
