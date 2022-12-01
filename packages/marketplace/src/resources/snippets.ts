1288b9c2b296aae16607687667e1bb22a0a58768
Rick <mcfly2340@gmail.com>
2022-12-01 08:36:36 +0100

    changed name of snippet

--- packages/marketplace/src/resources/snippets.ts
+++ packages/marketplace/src/resources/snippets.ts
@@ -180,9 +180,9 @@ export default [
     "preview": "resources/assets/snippets/remove-recently-played.png",
   },
   {
-    "title": "Modernize custom app icons",
+    "title": "Make custom app icons thicker",
     "description": "Gives icons from the sticky list a thicker border",
     "code": "#spicetify-sticky-list>li:nth-child(1n+1)>a>div.icon.collection-icon>svg { stroke: currentcolor; stroke-width: 11px; } .collection-icon { color: unset; }",
-    "preview": "resources/assets/snippets/modernize-sticky-list-icons.png",
+    "preview": "resources/assets/snippets/thicker-sticky-list-icons.png",
   },
 ];
