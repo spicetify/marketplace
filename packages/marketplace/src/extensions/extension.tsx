e09606b1c55ff8a5e67acdc15a495ac485b2be1e
Charlie <60494758+CharlieS1103@users.noreply.github.com>
2022-12-03 17:49:08 -0500

    Update packages/marketplace/src/extensions/extension.tsx
    Co-authored-by: Nam Anh <phamnamanh25@gmail.com>

--- packages/marketplace/src/extensions/extension.tsx
+++ packages/marketplace/src/extensions/extension.tsx
@@ -94,8 +94,7 @@ import {
       Spicetify.Config.color_scheme = themeManifest.activeScheme;
       if (localStorage.getItem(LOCALSTORAGE_KEYS.albumArtBasedColor) === "true") {
         initAlbumArtBasedColor(activeScheme);
-      }
-      else if (localStorage.getItem(LOCALSTORAGE_KEYS.colorShift) === "true") {
+      } else if (localStorage.getItem(LOCALSTORAGE_KEYS.colorShift) === "true") {
         initColorShiftLoop(themeManifest.schemes);
       }
     } else {
