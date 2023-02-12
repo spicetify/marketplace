b74034a7bd108b8dc0c93bd7839b5b64bfb85ce4
Isaac <padspambox@gmail.com>
2023-02-11 20:10:38 -0500

    fix: fix broken snippet card title links (#437)

--- src/constants.ts
+++ src/constants.ts
@@ -38,7 +38,7 @@ export const CUSTOM_APP_PATH = "/marketplace";
 export const MAX_TAGS = 4;
 
 export const SNIPPETS_PAGE_URL =
-	"https://github.com/spicetify/spicetify-marketplace/blob/main/packages/marketplace/src/resources/snippets.ts";
+	"https://github.com/spicetify/spicetify-marketplace/blob/main/src/resources/snippets.ts";
 
 export const BLACKLIST_URL =
 	"https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/blacklist.json";
