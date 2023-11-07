5dc6e51d4b12383dc9264c8095715094ac3af6a3
Grigory <techuila.sunset@gmail.com>
2023-11-07 10:40:00 +0500

    remove ecmaVersion from parserOptions
    env es2022 is enough

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
   ],
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
-    "ecmaVersion": "2022",
     "sourceType": "module",
     "project": ["tsconfig.json"],
   },
