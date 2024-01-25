710a5176c72f01aae8a1f9ecb9835111400af8da
Grigory <techuila.sunset@gmail.com>
2024-01-25 20:38:16 +0500

    style: move locales import to separate file

---
+++ src/resources/locales/index.ts
@@ -0,0 +1,22 @@
+import ca from "./ca.json";
+import en from "./en.json";
+import enUS from "./en-US.json";
+import es from "./es.json";
+import fr from "./fr.json";
+import zhTW from "./zh-TW.json";
+import zhCN from "./zh-CN.json";
+import ru from "./ru.json";
+import et from "./et.json";
+
+export default {
+  ca,
+  en,
+  "en-US": enUS,
+  es,
+  fr,
+  ru,
+  "zh-TW": zhTW,
+  "zh-CN": zhCN,
+  et,
+};
+
