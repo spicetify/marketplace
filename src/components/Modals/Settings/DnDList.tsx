7f0a7ad2cefb3b064d47ca6435940bd0e87150f8
zhr1a2002 <haorui.zheng@tum.de>
2024-06-24 17:58:33 +0200

    Add disable logic

--- src/components/Modals/Settings/DnDList.tsx
+++ src/components/Modals/Settings/DnDList.tsx
@@ -3,15 +3,27 @@ import { DragDropContext, Draggable, type DropResult, Droppable } from "react-be
 import { LOCALSTORAGE_KEYS } from "../../../constants";
 import type { Config, TabItemConfig } from "../../../types/marketplace-types";
 
+
+
+
 const DnDList = (props: {
   modalConfig: Config;
   updateConfig: (CONFIG: Config) => void;
 }) => {
-  const getItemStyle = (isDragging, draggableStyle) => ({
+  // Get Value of CSS variable
+  const colorVariable = getComputedStyle(document.body).getPropertyValue("--spice-button-disabled")
+
+  const getItemStyle = (isDragging, draggableStyle, isEnabled) => ({
+    borderRadius: "5px",
+    border: isEnabled ? `2px solid ${colorVariable}` : "2px solid red",
     userSelect: "none",
     padding: 16,
     margin: "0 8px 0 0",
-    background: isDragging ? "none" : "2px solid var(--spice-button-disabled);",
+    width: "110px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    textDecoration: isEnabled ? "none" : "line-through",
     ...draggableStyle
   });
 
@@ -40,6 +52,17 @@ const DnDList = (props: {
     props.updateConfig(props.modalConfig);
   }
 
+  const onToggleEnabled = (name) => {
+    const updatedTabs = props.modalConfig.tabs.map(tab =>
+      tab.name === name ? { ...tab, enabled: !tab.enabled } : tab
+    );
+
+    props.modalConfig.tabs = updatedTabs;
+    localStorage.setItem(LOCALSTORAGE_KEYS.tabs, JSON.stringify(props.modalConfig.tabs));
+    props.updateConfig(props.modalConfig);
+  };
+
+
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Droppable droppableId="droppable" direction="horizontal">
@@ -52,7 +75,8 @@ const DnDList = (props: {
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
-                    style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
+                    style={getItemStyle(snapshot.isDragging, provided.draggableProps.style, item.enabled)}
+                    onClick={() => onToggleEnabled(item.name)}
                   >
                     {item.name}
                   </div>
